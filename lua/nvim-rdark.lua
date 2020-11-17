1b915cfb2affe2aa17df5f8535212bd33de3daca
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-11-17 22:57:05 +0530

    Added todo highlighting

--- lua/nvim-rdark.lua
+++ lua/nvim-rdark.lua
@@ -1,6 +1,13 @@
 local Color, colors, Group, _, styles = require('colorbuddy').setup()
 local no = styles.NONE
 
+
+
+
+--------------------
+---Color Pallete----
+--------------------
+
 Color.new('white',           '#f2e5bc')
 Color.new('red',             '#cf4a4a')
 Color.new('pink',            '#fef601')
@@ -21,6 +28,14 @@ Color.new('syntax_fg',       '#8c8c8c')
 Color.new('comment_fg',      '#454545')
 Color.new('cursor_line_bg',  '#181b1f')
 
+
+
+
+
+-----------------------
+--Editor Highlighting--
+-----------------------
+
 Group.new('ColorColumn',   colors.none,            colors.cursor_line_bg,  no)
 Group.new('Normal',        colors.syntax_fg,       colors.syntax_bg,       no)
 Group.new('VertSplit',     colors.brown,           colors.syntax_bg,       no)
@@ -28,7 +43,6 @@ Group.new('TabLineFill',   colors.yellow,          colors.turquoise,       no)
 Group.new('CursorLineNr',  colors.yellow,          colors.cursor_line_bg,  styles.BOLD)
 Group.new('LineNr',        colors.syntax_fg,       colors.syntax_bg,       no)
 Group.new('StatusLine',    colors.black,           colors.blue,            no)
-Group.new('Comment',       colors.comment_fg,      colors.syntax_bg,       no)
 Group.new('SignColumn',    colors.white,           colors.syntax_bg,       no)
 Group.new('MatchParen',    colors.white,           colors.grey,            no)
 Group.new('CursorLine',    colors.none,            colors.cursor_line_bg,  no)
@@ -37,3 +51,15 @@ Group.new('Pmenu',         colors.syntax_fg,       colors.cursor_line_bg,  no)
 Group.new('TabLine',       colors.syntax_fg,       colors.cursor_line_bg,  no)
 Group.new('TabLineFill',   colors.white,           colors.cursor_line_bg,  no)
 Group.new('TabLineSel',    colors.cursor_line_bg,  colors.blue,            no)
+
+
+
+
+
+
+--------------------------------
+--Standard Syntax Highlighting--
+--------------------------------
+
+Group.new('Comment',  colors.comment_fg,  colors.syntax_bg,  no)
+Group.new('Todo',     colors.blue,        colors.syntax_bg,  styles.BOLD)
